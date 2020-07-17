7849e25aac7126f796004172c62fd2f7f6f7befb
Carlos Azaustre <cazaustre@gmail.com>
2020-07-18 00:24:15 +0200

    list launches

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema');
 
 const PORT = process.env.PORT || 5000
 const app = express();
 
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema,
   graphiql: true,
