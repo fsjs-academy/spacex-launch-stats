a0ce1d91fa21295d77f317d6ade3eb5aad229dfe
Carlos Azaustre <cazaustre@gmail.com>
2020-07-18 00:25:56 +0200

    add client src

---
+++ client/src/components/Launches.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+import { LaunchItem } from './LaunchItem'
+
+const LAUNCHES_QUERY = gql`
+  query LaunchesQuery {
+    launches {
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
+    }
+  }
+`;
+
+export const Launches = () => {
+  return (
+    <>
+      <h1 className="display-4 my-3">Launches</h1>
+      <Query query={LAUNCHES_QUERY}>
+        {
+          ({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>
+            if (error) console.log(error);
+
+            return <>
+              {
+                data.launches.map(launch => (
+                  <LaunchItem key={launch.flight_number} launch={launch} />
+                ))
+              }
+            </>
+          }
+        }
+      </Query>
+    </>
+  )
+}
