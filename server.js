9edfe353829a13d7a20ce510edbf1cd7d299ae8a
Carlos Azaustre <cazaustre@gmail.com>
2020-07-18 17:51:16 +0200

    🐛 fix ports and uris

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 const schema = require('./schema');
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(cors());
