f68ab21ad3562f670299e8bddb78901194906684
Carlos Azaustre <cazaustre@gmail.com>
2020-07-18 13:01:47 +0200

    added styles touches

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import classNames from 'classnames'
+import Moment from 'react-moment'
 
 export const LaunchItem = ({
   launch: {
@@ -12,8 +14,15 @@ export const LaunchItem = ({
     <div className="card card-body mb-3">
       <div className="row">
         <div className="col-md-9">
-          <h4>Mission: {mission_name}</h4>
-          <p>Date: {launch_date_local}</p>
+          <h4>Mission:{' '}
+            <span className={classNames({
+              'text-success': launch_success,
+              'text-danger': !launch_success,
+            })}>
+              {mission_name}
+            </span>
+          </h4>
+          <p>Date: <Moment format="YYYY-MM-DD HH:mm">{launch_date_local}</Moment></p>
         </div>
         <div className="col-md-3">
           <button className="btn btn-secondary">Launch Details</button>
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
 
 import { LaunchItem } from './LaunchItem'
+import { MissionKey } from './MissionKey'
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -19,6 +20,7 @@ export const Launches = () => {
   return (
     <>
       <h1 className="display-4 my-3">Launches</h1>
+      <MissionKey />
       <Query query={LAUNCHES_QUERY}>
         {
           ({ loading, error, data }) => {
